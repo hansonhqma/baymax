152712f50bf396505bdc1930e33c8e5da5019d3d
Hanson Ma <hanson.hq.ma@gmail.com>
2022-10-25 23:32:52 -0400

    ros msg names

--- smbus_manual_control/node.py
+++ smbus_manual_control/node.py
@@ -5,6 +5,12 @@ import rospy
 from std_msgs.msg import Bool
 from std_msgs.msg import Float32MultiArray
 
+# ROSpy node/msg names
+NODE_NAME = "smbus_node"
+READ_JOINT_MSG = "/base/read_joints"
+SET_JOINT_MSG = "/base/set_joints"
+SET_TORQUE_MSG = "/base/set_torque"
+
 def handle_read_angles( angles ):
     msg = Float32MultiArray()
     msg.data = tuple( angles )
@@ -21,10 +27,10 @@ def handle_write_angles( msg ):
 def handle_setting_torque( msg ):
     arm.set_torque( msg.data )
 
-rospy.init_node( "smbus_node" )
-pub = rospy.Publisher( '/read_joints', Float32MultiArray, queue_size=10 )
-rospy.Subscriber( "/set_joints", Float32MultiArray, handle_write_angles )
-rospy.Subscriber( "/set_torque", Bool, handle_setting_torque )
+rospy.init_node( NODE_NAME )
+pub = rospy.Publisher( READ_JOINT_MSG, Float32MultiArray, queue_size=10 )
+rospy.Subscriber( SET_JOINT_MSG, Float32MultiArray, handle_write_angles )
+rospy.Subscriber( SET_TORQUE_MSG, Bool, handle_setting_torque )
 
 arm = driver.Arm_Dev()
 
