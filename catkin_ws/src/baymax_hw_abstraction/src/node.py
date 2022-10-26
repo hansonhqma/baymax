7d56cfcc70694dbbcf4d16647c16d4e55cb409ff
Hanson Ma <hanson.hq.ma@gmail.com>
2022-10-26 19:10:34 -0400

    added velocity control to baymax_hw_abstraction

--- catkin_ws/src/baymax_hw_abstraction/src/node.py
+++ catkin_ws/src/baymax_hw_abstraction/src/node.py
@@ -7,14 +7,21 @@ from std_msgs.msg import Bool
 from std_msgs.msg import Float32MultiArray
 
 # ROSpy node/msg names
+
 NODE_NAME = "smbus_node"
 READ_JOINT_MSG = "/base/read_joints"
 SET_JOINT_MSG = "/base/set_joints"
 SET_TORQUE_MSG = "/base/set_torque"
+SET_JOINTS_VEL_MSG = '/base/set_joints_vel'
+
+CURRENT_ANGLES = None
+VEL_SIGNATURE = None
 
 def handle_read_angles( angles ):
+    global CURRENT_ANGLES
+    CURRENT_ANGLES = angles
+
     msg = Float32MultiArray()
-    msg.data = tuple( angles )
     pub.publish( msg )
 
 def handle_write_angles( msg ):
@@ -27,14 +34,37 @@ def handle_write_angles( msg ):
     else:
         print( "Error, angle message invalid" )
 
+def handle_write_vel( msg ):
+    global CURRENT_ANGLES
+    global VEL_SIGNATURE
+    VEL_SIGNATURE = msg.data
+    if msg.data = [0.0]*6: # stop
+        return:
+
+    print("new vel signer:", VEL_SIGNATURE)
+
+    if( len( msg.data ) == 6 ):
+        for angle in msg.data:
+            if( type( angle ) != float ):
+                print( "Error, velocity type invalid" )
+                return
+        while VEL_SIGNATURE == msg.data: # while i am still the top caller
+                new_angles = [msg.data[i] + CURRENT_ANGLES[i] for i in range(6)]
+                arm.set_joints( new_angles )
+                rate.sleep()
+    else:
+        print( "Error, velocity message invalid" )
+    
+
 def handle_setting_torque( msg ):
     arm.set_torque( msg.data )
 
 rospy.init_node( NODE_NAME )
+rate = rospy.Rate(50)
 pub = rospy.Publisher( READ_JOINT_MSG, Float32MultiArray, queue_size=10 )
 rospy.Subscriber( SET_JOINT_MSG, Float32MultiArray, handle_write_angles )
+rospy.Subscriber( SET_JOINTS_VEL_MSG, Float32MultiArray, handle_write_vel )
 rospy.Subscriber( SET_TORQUE_MSG, Bool, handle_setting_torque )
-
 arm = driver.Arm_Dev()
 
 try:
