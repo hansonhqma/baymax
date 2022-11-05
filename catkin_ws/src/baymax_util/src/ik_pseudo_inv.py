b99ec80eb60843659855ac0c3e00b24c41eb6ce8
Ryan Walton <ryan.walton.256@gmail.com>
2022-11-05 18:18:29 -0400

    Fix ik alg

--- catkin_ws/src/baymax_util/src/ik_pseudo_inv.py
+++ catkin_ws/src/baymax_util/src/ik_pseudo_inv.py
@@ -6,21 +6,26 @@ from std_msgs.msg import Float32MultiArray
 import time
 import math
 
-alpha = 0.5
-iterations = 10000
+alpha = 0.4
+iterations = 70
 
 class robot:
     def __init__( self, _aAxes: np.ndarray, _aTranslations: np.ndarray ):
+        # Make sure each argument is the right size matrix
         assert( _aAxes.shape[0] == _aTranslations.shape[0] )
         assert( _aAxes.shape[1] == 3 )
         assert( _aTranslations.shape[1] == 3 )
+        # Save robot specific attributes
         self.__mNumJoints = _aAxes.shape[0]
         self.__mAxes = _aAxes
         self.__mTranslations = _aTranslations
 
     def fk( self, _aAngles: np.ndarray ):
+        # Make sure the size of the angles vector is correct
         assert( _aAngles.shape[0] == self.__mNumJoints )
 
+        # Create the rotation matrices between each joint from
+        # the axis angle representation
         R_ij = Rotation.from_rotvec(
             np.multiply(
                 np.asarray( [ _aAngles ] * 3 ).T,
@@ -28,10 +33,13 @@ class robot:
             )
         )
 
+        # Multiply each rotation to find the rotation matrix from
+        # the base frame to the end effector frame
         R_0i = R_ij
         for i in range( 1, self.__mNumJoints ):
             R_0i[i] = R_0i[ i-1 ] * R_ij[ i ]
 
+        # Determine the translation vectors to each joint in this configuraiton
         P_0i = np.ndarray( ( self.__mNumJoints, 3 ) )
         P_0i[0] = self.__mTranslations[0]
         for i in range( 1, self.__mNumJoints ):
@@ -40,72 +48,74 @@ class robot:
                 R_0i[ i-1 ].apply( self.__mTranslations[i] )
             )
 
+        # Return the rotation matrices and translation vectors
         return R_0i, P_0i
-    
+
+    # Returns the Jacobian for a given configuration    
     def Jq( self, _aAngles: np.ndarray ):
+        # Perform the forward kinematics
         R_0i, P_0i = self.fk( _aAngles )
+        # Find the axes of each joint in this configuration
         hi_0 = np.ndarray( ( self.__mNumJoints, 3 ) )
         hi_0[0] = self.__mAxes[0]
         for i in range( 1, self.__mNumJoints ):
             hi_0[i] = R_0i[i-1].apply( self.__mAxes[i] )
-        
+
+        # Find the distance from each joint to the end effector        
         P_iT_0 = np.ndarray( ( self.__mNumJoints, 3 ) )
         for i in range( 0, self.__mNumJoints ):
             P_iT_0[i] = P_0i[-1] - P_0i[i]
         
-        Jq = np.ndarray( ( self.__mNumJoints, 6 ) )
+        # Generate the Jacobian
+        Jq = np.ndarray( ( 6, self.__mNumJoints ) )
         for i in range( 0, self.__mNumJoints ):
-            Jq[i,0:3] = hi_0[i]
-            Jq[i,3:6] = np.cross( hi_0[i], P_iT_0[i] )
+            Jq[0:3,i] = hi_0[i]
+            Jq[3:6,i] = np.cross( hi_0[i], P_iT_0[i] )
 
         return Jq, R_0i, P_0i
 
-    def ik_pseudo_inv( self, _aTargetRot, _aTargetTrans, _aStartingAngles ):
+    def ik( self, _aTargetRot, _aTargetTrans, _aStartingAngles ):
         angles = _aStartingAngles
-        print( angles )
         for i in range( 0, iterations ):
-            msg = Float32MultiArray()
-            msg.data = tuple( angles ) + (0,)
-            print("q")
-            print( msg.data )
-            pub.publish( msg )
-
+            # Perform forward kinematics and get the jacobian for
+            # this configuration
             Jq, R_0i, P_0i = self.Jq( angles )
+            # Calculate the error in rotation matrices
             R_err = R_0i[-1] * _aTargetRot.inv()
-            print( P_0i )
-
-            """
-            print("Rotations:")
-            print( R_0i.as_rotvec() )
-            print( R_0i[-1:].as_rotvec() )
-            print( _aTargetRot.as_rotvec() )
-            print(R_err.as_rotvec())
-            """
-
+            # Calculate the error in translation
             P_err = P_0i[-1] - _aTargetTrans
+            # Put both of these values into a single error vector
             dx = np.ndarray( (6,) )
             dx[0:3] = R_err.as_rotvec()
             dx[3:6] = P_err
 
-            print("Translations")
-            print(_aTargetTrans)
-            print(P_0i[-1])
-            print(P_err)
-            print("dx")
-            print( dx )
-            print("matmul")
-            print( np.matmul( Jq, dx ) )
-            print("Jq")
-            print( Jq )
+            # Perform the "pseudo inverse" of the jacobian with the
+            # damped least squares method
+            inv = np.matmul(
+                Jq.T,
+                np.linalg.inv(
+                    np.add(
+                        np.matmul( Jq, Jq.T ),
+                        0.001 * np.identity( Jq.shape[0] )
+                    )
+                )
+            )
 
+            # Update angles according to the update matrix
+            # and the error matrix
             angles = np.add(
                 angles,
-                - alpha * np.matmul( Jq, dx )
+                - alpha * np.matmul( 
+                    #np.linalg.pinv( Jq ),
+                    inv,
+                    dx )
             )
-            print(0.1)
+        
+        print( "Error: " + str( np.linalg.norm( dx ) ) )
 
-        pass
+        return angles
 
+# Initialize the robot with the corresponding joints etc
 r = robot(
     np.array(
       [ [ 0,  0, 1 ],
@@ -121,18 +131,29 @@ r = robot(
         [ 0, 0, 0.12842 ] ] )
 )
 
+# Ros initialization
 NODE_NAME = "foo"
 SET_JOINT_MSG = "/base/set_joints"
 
 rospy.init_node( NODE_NAME )
 pub = rospy.Publisher( SET_JOINT_MSG, Float32MultiArray, queue_size=10 )
 
-print( r.Jq( np.array( [ math.pi, math.pi/2, -math.pi/2, math.pi/2, -math.pi ] ) ) )
+# Perform inverse kinematics
+rate = rospy.Rate(10)
 
-rot, trans = r.fk( np.array( [0,0,0,0,0] ) )
+count = 0
+angles = np.array( [0,0,0,0,0] )
+while True:
+    angles = r.ik(
+        Rotation.from_rotvec( [ 0.3 * math.cos( count / 10 ), -0.3 * math.sin( count / 10 ), 0 ] ),
+        #Rotation.from_rotvec( [ 0, 0, -count / 10 ] ),
+        #np.array( [ 0.15 * math.cos( count / 10 ), 0.15 * math.sin( count / 10 ), 0.25 + 0.05 * math.sin( count / 7 ) ] ),
+        np.array( [ 0, 0, 0.35 + 0.03 * math.sin( count / 7 ) ] ),
+        angles
+    )
+    msg = Float32MultiArray()
+    msg.data = tuple( angles ) + (0,)
+    pub.publish( msg )
+    count += 1
+    rate.sleep()
 
-r.ik_pseudo_inv(
-    Rotation.from_rotvec( np.array( [ 0, 1.57, 0 ] ) ),
-    np.array( [ 0.3, 0, 0.01 ] ),
-    np.array( [ 0, 0.1, -0.2, 0.05, 0 ] )
-)
